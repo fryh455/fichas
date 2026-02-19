78c8a29405fb82e0ba1a4defa551c30a3efbb14e
Níckollas <149337698+fryh455@users.noreply.github.com>
2026-02-19 20:37:44 -0300

    Add files via upload

--- fichas-online/js/firebase.js
+++ fichas-online/js/firebase.js
@@ -27,13 +27,14 @@ import {
  * - Project Settings -> SDK setup and configuration -> "Config"
  */
 export const FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBjSCYNOngXOSQGBU7jMj1kgf7hunfMjyI",
+  authDomain: "marionetes-do-destino.firebaseapp.com",
+  databaseURL: "https://marionetes-do-destino-default-rtdb.firebaseio.com",
+  projectId: "marionetes-do-destino",
+  storageBucket: "marionetes-do-destino.firebasestorage.app",
+  messagingSenderId: "506859529879",
+  appId: "1:506859529879:web:aef41f525b22754c7f6bd2",
+  measurementId: "G-LQNNS93LDY"
 };
 
 function assertConfig() {
